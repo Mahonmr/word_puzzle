d5c4a0af0b5e098484fc2f75d3061ce84bd909fd
Mike Mahoney <student@epicodus.com>
2016-01-08 11:36:23 -0800

    This commit show the method passes basic testing

--- spec/word_puzzle_spec.rb
+++ spec/word_puzzle_spec.rb
@@ -5,4 +5,28 @@ describe('Replace vowels with the dash symbol') do
   it 'will replace vowel with dash for one word one vowel' do
     expect('bad'.word_puzzle()).to(eq('b-d'))
   end
+
+  it 'will replace vowel with dash for one word mutliple vowels' do
+    expect('dude'.word_puzzle()).to(eq('d-d-'))
+  end
+
+  it 'will replace vowel with dash for mutliple words one vowel' do
+    expect('bad dog bad cat'.word_puzzle()).to(eq('b-d d-g b-d c-t'))
+  end
+
+  it 'will replace vowel with dash for one mutliple words and mutliple vowels' do
+    expect('good dog good cat'.word_puzzle()).to(eq('g--d d-g g--d c-t'))
+  end
+
+  it 'will replace upper case vowels with a dash for a word' do
+    expect('About'.word_puzzle()).to(eq('-b--t'))
+  end
+
+  it 'will replace mutliple upper case vowels with a dash for a word' do
+    expect('AbOut'.word_puzzle()).to(eq('-b--t'))
+  end
+
+  it 'will replace repeating vowels in a word' do
+    expect('good'.word_puzzle()).to(eq('g--d'))
+  end
 end
