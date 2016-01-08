3263273fdf0c8e1891fb5e7025fa8e0de05e2fc1
Mike Mahoney <student@epicodus.com>
2016-01-08 11:28:55 -0800

    This commit will set up structure and first test

---
+++ spec/word_puzzle_spec.rb
@@ -0,0 +1,7 @@
+require('rspec')
+require('word_puzzle')
+
+describe('Replace vowels with the dash symbol') do
+  it 'will replace vowel with dash for one word one vowel' do
+    expect('bad'.word_puzzle()).to(eq('b-d'))
+  end
