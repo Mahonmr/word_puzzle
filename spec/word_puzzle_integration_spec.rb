54a186c5faec3e12658888f5166e0e9ce34d35dd
Mike Mahoney <student@epicodus.com>
2016-01-08 16:22:41 -0800

    This commit corrected some code style

--- spec/word_puzzle_integration_spec.rb
+++ spec/word_puzzle_integration_spec.rb
@@ -7,7 +7,7 @@ set(:show_exceptions, false)
 describe('Flow of word puzzle game', {:type => :feature}) do
   it('takes you to the root path') do
     visit('/')
-    expect(page).to have_content("Welcome To Word Puzzle")
+    expect(page).to have_content('Welcome To Word Puzzle')
   end
 
   it('fills in string and displays puzzle') do
